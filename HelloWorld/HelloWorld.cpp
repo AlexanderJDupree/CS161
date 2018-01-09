2f79f06ca9ef814462ba92e2bf7869c082f9a8a1
Alex DuPree <Alexander.j.dupree@gmail.com>
2018-01-08 16:16:07 -0800

    C++ Hello World Project file
    Created standard HelloWorld program in C++

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,15 @@
+// HelloWorld.cpp
+// Description: Hello World program in C++
+// Author: Alexander DuPree
+// Date: 201801018
+// Compiler: Code::Blocks 17.12
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
