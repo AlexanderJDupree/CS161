89d9ba8d0435c1f8fdb4f32d912fa6477ae65e45
F14 <F14@LABS.MHCC.CC.OR.US>
2018-01-09 11:24:11 -0800

    Week 1 programs

---
+++ wk1_escape_adupree/wk1_escape_adupree.cpp
@@ -0,0 +1,22 @@
+/*********************************************************************
+* File: wk1_escape_adupree.cpp                                       *
+* Description: Prints a quote using escape sequences to console      *
+* Author: Alexander DuPree                                           *
+* Date: 1/9/2018                                                     *
+* Compiler: GNU CC Compiler                                          *
+* Modifications:                                                     *
+*********************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Our greatest weakness lies in giving up.\n"
+         << "The most certain way to success is\n"
+         << "to\ttry\tjust\tone\tmore\ttime.\n\n"
+         << "Thomas A. Edison\n\n"
+         << "https://www.breainyquote.com/\n"
+         << endl;
+    return 0;
+}
---
+++ wk1_picture_adupree/wk1_pciture_adupree.cpp
@@ -0,0 +1,24 @@
+/*********************************************************************
+* File: wk1_picture_adupree.cpp                                      *
+* Description: Prints ASCII picture to console                       *
+* Author: Alexander DuPree                                           *
+* Date: 1/9/2018                                                     *
+* Compiler: GNU CC Compiler                                          *
+* Modifications:                                                     *
+*********************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << " XXX           XXX\n"
+         << "XXXX XXXXXXXXX XXXX\n"
+         << "to\ttry\tjust\tone\tmore\ttime.\n\n"
+         << "Thomas A. Edison\n\n"
+         << "https://www.breainyquote.com/\n"
+         << endl;
+    return 0;
+}
+
+/* Needs special ascii art with three different charracters and stuff */
