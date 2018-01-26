abe2d1e960dd633290f5ce9199119d1d8cfc09cd
Alex DuPree <alexander.j.dupree@gmail.com>
2018-01-26 12:31:05 -0800

    Removed stoi() function from saints pizza program

--- week2/wk2_saintsPizza_adupree/wk2_saintsPizza_adupree.cpp
+++ week2/wk2_saintsPizza_adupree/wk2_saintsPizza_adupree.cpp
@@ -3,8 +3,13 @@
 * Description: Displays pizza sales info for saints pizza            *
 * Author: Alexander DuPree                                           *
 * Date: 1/16/2018                                                    *
-* Compiler: Code::Blocks 17.12                                       *
-* Modifications:                                                     *
+* Compiler: Code::Blocks 16.01                                       *
+* Modifications: Moved main to top of program. simplified the        *
+*                getPizzasSoldBySize function to not accomodate error*
+*                checking because I could not find a way to convert  *
+*                a string to an int without using stoi(). If user    *
+*                decides to enter a non-numeric character the program*
+*                will now just quit.                                 *
 *********************************************************************/
 
 #include <iostream>
@@ -18,62 +23,6 @@ void intro();
 void displayResults(const int, const vector<pair<string, int>>);
 int getPizzaSoldBySize(const string);
 bool isValidNum(const string);
-
-void intro()
-{
-    cout << "\t\tSAINTS PIZZA SALES" << endl << endl;
-    cout << "Enter the number of each type of pizza sold this month.";
-    cout << endl;
-}
-
-int getPizzaSoldBySize(const string pizzaSize)
-{
-    /* A string is used here to store user input because we need to
-    iterate through each character of the input to ensure the input
-    is a valid number */
-    string pizzasSold;
-
-    bool isInt = false;
-    do {
-        cout << pizzaSize << ":  ";
-        getline(cin, pizzasSold);
-        isInt = isValidNum(pizzasSold);
-    }while (!isInt);
-
-    // converts string to int
-    return stoi(pizzasSold);
-}
-
-bool isValidNum(const string userInput)
-{
-    for (char c : userInput)
-    {
-        if (isalpha(c) || c == '.')
-        {
-            cout << userInput << " is invalid. Enter a whole number."
-                 << endl;
-            return false;
-        }
-    }
-    return true;
-}
-
-void displayResults(const int totalSold,
-                    const vector<pair<string, int>> pizzasSoldBySize)
-{
-    cout << "\n\nTotal number of pizzas sold:  " << totalSold << endl;
-    cout << "\nPercentage of total for each size:" << endl;
-
-    for (auto pizzaSize : pizzasSoldBySize)
-    {
-        float percentOfTotal = (pizzaSize.second /
-                                static_cast<float>(totalSold)) * 100;
-
-        cout << setw(6) << left << pizzaSize.first << '\t'
-             << setw(6) << right << setprecision(1)
-             << fixed << percentOfTotal  << endl;
-    }
-}
 
 int main()
 {
@@ -92,5 +41,42 @@ int main()
     displayResults(totalSold, pizzasSoldBySize);
 
     return 0;
+}
+
+void intro()
+{
+    cout << "\t\tSAINTS PIZZA SALES" << endl << endl;
+    cout << "Enter the number of each type of pizza sold this month.";
+    cout << endl;
+}
+
+int getPizzaSoldBySize(const string pizzaSize)
+{
+    /* Changed pizzasSold from string to int, I won't be conducting error
+    checking because I could not figure out a solid work around for converting
+    a string to an int without using stoi() */
+    int pizzasSold;
+
+    cout << pizzaSize << ":  ";
+    cin >> pizzasSold;
+
+    return pizzasSold;
+}
+
+void displayResults(const int totalSold,
+                    const vector<pair<string, int>> pizzasSoldBySize)
+{
+    cout << "\n\nTotal number of pizzas sold:  " << totalSold << endl;
+    cout << "\nPercentage of total for each size:" << endl;
+
+    for (auto pizzaSize : pizzasSoldBySize)
+    {
+        float percentOfTotal = (pizzaSize.second /
+                                static_cast<float>(totalSold)) * 100;
+
+        cout << setw(6) << left << pizzaSize.first << '\t'
+             << setw(6) << right << setprecision(1)
+             << fixed << percentOfTotal  << endl;
+    }
 }
 
